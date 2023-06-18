d5cf399b9c9fe4bbb9b4cac1d2b6ed26417b0853
matealomot <94622393+matealomot@users.noreply.github.com>
2023-06-18 23:25:59 +0200

    Delete React directory

--- React/my-weather-app/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-    return (
-        <footer>
-            <p className="tips"><b>Tip:</b> To make the search more precise put the city's name, comma, 2-letter country code (ISO3166).</p>
-        </footer>
-    )
-}
