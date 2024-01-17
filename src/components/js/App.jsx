eccf7e37656b301cfad05ae4fa3e2474576faa7e
matealomot <miss.mitic@protonmail.com>
2024-01-17 21:05:26 +0100

    Added a simple loading message on screen for when waiting for data to be fetched and displayed

--- src/components/js/App.jsx
+++ src/components/js/App.jsx
@@ -7,15 +7,21 @@ import Footer from './Footer';
 function App() {
 
   const [query, setQuery] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleSearch(requiredInput) {
     setQuery(requiredInput.length > 0 ? requiredInput : "No city found / No data available")
+    setIsLoading(false);
+  }
+
+  function handleLoading() {
+    setIsLoading(true);
   }
 
   return (
     <>
-      <Header handleSearch={handleSearch}/>
-      <Body weatherData={query}/>
+      <Header handleSearch={handleSearch} handleLoading={handleLoading}/>
+      <Body weatherData={query} isLoading={isLoading}/>
       <Footer />
     </>
   )
